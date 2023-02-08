5611f31d7aa07be7e1d284c17d1a361435402e4e
Marko <Piirmarko92@gmail.com>
2023-02-08 21:06:36 +0200

    List task solutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Teachers} from "../shared/modules/teachers";
+import {Students} from "../shared/modules/students";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-list-task';
+
+
+  teacher: Teachers[] = [];
+
+  teacherNames: string[] = ["Marko", "John", "Bruno", "Jim"];
+  teacherLastNames: string[] = ["Piir", "Smith", "Musk", "Branson"];
+  teacherAge: string[] = ["20", "22", "23", "65"]
+  teacherEmail: string[] = ["js@gmail.com", "mp@gmail.com", "bm@gmail.com", "jb@gmail.com"]
+
+  fetchTeachers(): Teachers[] {
+    let teacher: Teachers[] = [];
+
+    teacher.push(new Teachers("John", "Smith", "30", "js@gmail.com"));
+
+
+    return teacher;
+  }
+
+  alertBox() {
+    alert("wss")
+  }
+
+  addTeacher() {
+    let teacherName = this.teacherNames.at(Math.floor(Math.random() * this.teacher.length));
+    let teacherLastName = this.teacherLastNames.at(Math.floor(Math.random() * this.teacher.length));
+    let teacherAges = this.teacherAge.at(Math.floor(Math.random() * this.teacher.length));
+    let teacherEmails = this.teacherEmail.at(Math.floor(Math.random() * this.teacher.length));
+
+    // @ts-ignore
+    this.teacher.push(new Teachers(teacherName, teacherLastName, teacherAges, teacherEmails));
+  }
+
+  student: Students[] = [];
+
+  studentNames: string[] = ["Mark", "Mike", "Clark", "Ringo"];
+  studentLastNames: string[] = ["Musk", "Suva", "Buffet", "Roosaare"];
+  studentAge: string[] = ["55", "45", "21", "60"]
+  studentEmail: string[] = ["mm@gmail.com", "kl@gmail.com", "zx@gmail.com", "qw@gmail.com"]
+
+  addStudent() {
+    let studentName = this.studentNames.at(Math.floor(Math.random() * this.teacher.length));
+    let studentLastName = this.studentLastNames.at(Math.floor(Math.random() * this.teacher.length));
+    let studentAges = this.studentAge.at(Math.floor(Math.random() * this.teacher.length));
+    let studentEmails = this.studentEmail.at(Math.floor(Math.random() * this.teacher.length));
+
+    // @ts-ignore
+    this.teacher.push(new Teachers(teacherName, teacherLastName, teacherAges, teacherEmails));
+  }
+
+
 }
---
+++ src/shared/modules/students.ts
@@ -0,0 +1,19 @@
+export class Students {
+
+  firstName: string;
+
+  lastName: string;
+
+  age: string;
+
+  email: string;
+
+
+
+  constructor(firstName: string, lastName: string, age: string, email: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.email = email;
+  }
+}
