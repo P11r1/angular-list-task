e36a9874474ee8ed499db6d21f4ca738bcbd1479
Marko <Piirmarko92@gmail.com>
2023-03-09 19:11:41 +0200

    Angular linking to spring

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  DefaultLanguage: 'en',
+  development: true,
+  environment: 'DEV',
+  baseURL: 'http://localhost:8080/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  DefaultLanguage: 'en',
+  development: false,
+  environment: 'PROD',
+  baseURL: 'http://localhost:8080/'
+};
